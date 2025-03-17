9b0edfdf4bea10fec8bdc9d47acf1442a6cd21da
Daoll <daoll43000@gmail.com>
2025-03-17 16:11:37 +0900

    updated

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # 2025-1pserver
 2025 1학기 프로젝트2, 붓꽃분류기 서버와 클라이언트 개발
 
+<img src="https://img.shields.io/badge/github-181717?style=for-the-badge&logo=github&logoColor=white">
+<img src="https://img.shields.io/badge/Python-3776AB?style=for-the-badge&logo=Python&logoColor=white">
+<img src="https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=JavaScript&logoColor=white">
 ## fastAPI 를 이용한 붓꽃 분류시스템을 차근 차근 만들어보자
 
 <img src= "irisphoto.png">
--- front/iris.js
+++ front/iris.js
@@ -4,7 +4,7 @@ function Send(){
     pl = document.getElementById("pl")
     pw = document.getElementById("pw")
 
-    alert("자바 스크립트 Send 함수를 불려짐")
+    alert("결과 불러옴")
   var data = {
     'sepal_length': sl.value,
     'sepal_width': sw.value,
@@ -21,7 +21,7 @@ function Send(){
      data: JSON.stringify(data),
     }).done(function(response){
         console.log(response)
-        txtOut.value = response.prediction
+        txtOut.value = response.prediction + " 일 확률 " + response.probability
     }).fail(function(response){
         alert("fail" +response)
     }
