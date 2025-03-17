858aab6032d9956665007daf9df3f7e5771b28b8
Daoll <daoll43000@gmail.com>
2025-03-17 15:34:28 +0900

    리드미

--- front/iris.js
+++ front/iris.js
@@ -1,39 +1,29 @@
 function Send(){
+    sl = document.getElementById("sl")
+    sw = document.getElementById("sw")
+    pl = document.getElementById("pl")
+    pw = document.getElementById("pw")
 
-sl = document.getElementById("sl")
-sw = document.getElementById("sw")
-pl = document.getElementById("pl")
-pw = document.getElementById("pw")
-
+    alert("자바 스크립트 Send 함수를 불려짐")
   var data = {
     'sepal_length': sl.value,
     'sepal_width': sw.value,
     'petal_length': pl.value,
     'petal_width': pw.value,
   }
-
-  $.ajax({
+ $.ajax({
     type: "POST",
     url: 'http://localhost:8000/predict',
     headers:{
         "Accept" : "application/json",
         "Content-Type": "application/json",
-        },
-    data: JSON.stringify(data),
-
-  }).done(function(response) {
-
-        txtOut.value = response.prediction + "  " + response.probability
-
+     },
+     data: JSON.stringify(data),
+    }).done(function(response){
         console.log(response)
-
-
-  }).fail(function(error) {
-    alert("!/js/user.js에서 에러발생: " + error.statusText);
-    console.log(error)
-  }).always(function(r){
-    console.log("always" + r)
-  });
-
-
-}
+        txtOut.value = response.prediction
+    }).fail(function(response){
+        alert("fail" +response)
+    }
+    ).always()
+  }
